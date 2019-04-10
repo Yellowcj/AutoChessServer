facc8e1c19c64553d9c31a8d870cf64b67f35de8
Yellowcj <295930301@qq.com>
2019-04-10 10:11:12 +0800

    initial upload

---
+++ src/test/java/edu/xmu/autochessserver/AutochessserverApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.xmu.autochessserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AutochessserverApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
